f30fd391dcc4918e27fe95dc850b53f1d389f3d6
Kate Bertelsen <kate@rememberautumn.com>
2020-05-21 15:33:45 -0500

    Create blank Xcode project

---
+++ StringUnhasher/StringUnhasher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StringUnhasher
+//
+//  Created by Kate Bertelsen on 5/21/20.
+//  Copyright © 2020 Kate Bertelsen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
