015a55cdc67a3c43bdc9940b0ea2b93e6f257fba
Kate Bertelsen <kate@rememberautumn.com>
2020-05-21 15:45:20 -0500

    Delete boilerplate

--- StringUnhasher/main.swift
+++ StringUnhasher/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  StringUnhasher
-//
-//  Created by Kate Bertelsen on 5/21/20.
-//  Copyright © 2020 Kate Bertelsen. All rights reserved.
-//
-
 import Foundation
 
 print("Hello, World!")
