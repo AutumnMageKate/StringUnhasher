6746342282a350b24b9f81993334b6e435e1f23a
Kate Bertelsen <kate@rememberautumn.com>
2020-05-21 15:56:03 -0500

    Add extension for computing a hash with the java algorithm

--- StringUnhasher/Extensions.swift
+++ StringUnhasher/Extensions.swift
@@ -1,9 +1,17 @@
-//
-//  Extensions.swift
-//  StringUnhasher
-//
-//  Created by Kate Bertelsen on 5/21/20.
-//  Copyright © 2020 Kate Bertelsen. All rights reserved.
-//
-
 import Foundation
+
+// Adapted from https://stackoverflow.com/questions/42677948/implementing-java-strings-hashcode-method-in-swift-3
+
+extension String {
+    var asciiValueArray: [UInt32] {
+        return unicodeScalars.filter{$0.isASCII}.map{$0.value}
+    }
+
+    var hashCode: Int32 {
+        var runningHash: Int32 = 0
+        for asciiValue in asciiValueArray {
+            runningHash = 31 &* runningHash &+ Int32(asciiValue) // Notice use of overflow operators
+        }
+        return runningHash
+    }
+}
--- Tests/ExtensionsTests.swift
+++ Tests/ExtensionsTests.swift
@@ -2,4 +2,10 @@ import XCTest
 
 class ExtensionsTests: XCTestCase {
 
+    func testHash() {
+        XCTAssertEqual("a".hashCode, Int32(97))
+        XCTAssertEqual("aa".hashCode, Int32(3104))
+        XCTAssertEqual("ab".hashCode, Int32(3105))
+        XCTAssertEqual("aaaaaa".hashCode, Int32(-1425372064))
+    }
 }
